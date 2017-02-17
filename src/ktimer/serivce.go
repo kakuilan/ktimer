831c56bc68511ca21d2ed14b8c436774d8c32c77
kakuilan <kakuilan@163.com>
2017-02-17 13:44:35 +0800

    redis conf

--- src/ktimer/serivce.go
+++ src/ktimer/serivce.go
@@ -2,22 +2,43 @@ package ktimer
 
 import (
 	"fmt"
-    "github.com/astaxie/beego/config"
+    //"github.com/astaxie/beego/config"
+    "config"
+    "gopkg.in/redis.v5"
 )
+
+//
+func GetRedisClient() (*redis.Client,error) {
+    var client *redis.Client
+    file := GetConfFilePath()
+    cnf,err := config.NewConfig(file)
+    if(err ==nil ) {
+      var addr string
+      host := cnf.String("redis::redis.host")
+      port := cnf.String("redis::redis.port")
+      pawd := cnf.String("redis::redis.passwd")
+      db,err2 := cnf.Int("redis::redis.db")
+      addr = host + ":" + port
+      fmt.Println(host,port,addr,pawd,db,err2, "redis conf")
+      
+      client = redis.NewClient(&redis.Options{
+          Addr: addr ,
+          Password : pawd,
+          DB: db,
+      })
+
+      fmt.Println(client)
+      return  client,err2
+    }else{
+    }
+
+    return client,err
+}
 
 //检查redis是否连接
 func CheckRedis() (bool,error){
-    file := GetConfFilePath()
-    cnf,err := config.NewConfig("ini", file)
-    if(err == nil) {
-        host := cnf.String("redis.host")
-        port := cnf.String("redis.post")
-        db := cnf.String("redis.db")
-
-        fmt.Println(host,port,db,"redis conf")
-    }
-
-    fmt.Println(cnf,err, "conf")
+    client,err := GetRedisClient()
+    fmt.Println(client)
 
     return true,err
 }
