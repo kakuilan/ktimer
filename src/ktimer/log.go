7226f33e4de8b6ed300192077e60585304fe3387
kakuilan <kakuilan@163.com>
2017-03-07 12:15:17 +0800

    log test

--- src/ktimer/log.go
+++ src/ktimer/log.go
@@ -1,54 +1,50 @@
 package ktimer
+
 import (
-    "gopkg.in/natefinch/lumberjack.v2"
-    "log"
+	//"fmt"
+	"gopkg.in/natefinch/lumberjack.v2"
+	//"reflect"
 )
 
 //全局日志对象
-var RunLoger,ErrLoger lumberjack.Logger = nil
+var RunLoger, ErrLoger *lumberjack.Logger
 
 //获取运行日志对象
-func GetRunLoger()(interface{}, error) {
-    var err error
-    var logdir string
-    if RunLoger == nil {
-        CnfObj, err = GetConfObj()
-        if err !=nil {
-            return  RunLoger,err
-        }
-        logdir,err = CheckLogdir()
-        if err !=nil  {
-           return RunLoger,err
-        }
-        
-        file :=  CnfObj.String("log::log.runed_file")
-        maxsize,err := CnfObj.Int("log::log.maxsize")
-        if err!=nil {
-            maxsize = 500
-        }
-        maxbackup,err := CnfObj.Int("log::log.maxbackup")
-        if err !=nil {
-            maxbackup = 5
-        }
-        maxage,err := CnfObj.Int("log::log.maxage")
-        if err != nil {
-            maxage = 30
-        }
+func GetRunLoger() (interface{}, error) {
+	var err error
+	if RunLoger == nil {
+		CnfObj, err = GetConfObj()
+		if err != nil {
+			return RunLoger, err
+		}
+		var logdir string
+		logdir, err = CheckLogdir()
+		if err != nil {
+			return RunLoger, err
+		}
 
-        RunLoger = &lumberjack.Logger{
-            Filename : file,
-            MaxSize : maxsize,
-            MaxBackups : maxbackup,
-            MaxAge : maxage,
-        }
-        println(RunLoger)
-        //log.SetOutput(RunLoger) 
-    }
+		file := CnfObj.String("log::log.runed_file")
+		maxsize, err := CnfObj.Int("log::log.maxsize")
+		if err != nil {
+			maxsize = 500
+		}
+		maxbackup, err := CnfObj.Int("log::log.maxbackup")
+		if err != nil {
+			maxbackup = 5
+		}
+		maxage, err := CnfObj.Int("log::log.maxage")
+		if err != nil {
+			maxage = 30
+		}
 
-    return RunLoger,err
-}
+		RunLoger = &lumberjack.Logger{
+			Filename:   logdir + "/" + file,
+			MaxSize:    maxsize,
+			MaxBackups: maxbackup,
+			MaxAge:     maxage,
+		}
 
-//获取错误日志对象
-func GetErrLoger(){
-    
+	}
+
+	return RunLoger, err
 }
--- src/main.go
+++ src/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	. "ktimer"
+    "fmt"
 )
 
 func main() {
 	//命令行处理
+    fmt.Println(111, RunLoger)
+    ll,err := GetRunLoger()
+    fmt.Println(222, ll,err)
     CatchCli()
 }
