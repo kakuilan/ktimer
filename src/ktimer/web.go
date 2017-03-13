1171202f734c5566bd2d42f1e8f7c7af7f33ea4a
kakuilan <kakuilan@163.com>
2017-03-13 18:37:20 +0800

    web

--- src/ktimer/web.go
+++ src/ktimer/web.go
@@ -14,16 +14,22 @@ func WebContainer() {
         ServiceError("web server get conf fail.", err)
     }
 
-    pwd := RandPwd(12,3)
-    pwdstr := string(pwd)
-    fmt.Println(pwd, pwdstr)
+    open,err := CnfObj.Int("web::web.enable")
+    if err!=nil {
+        ServiceError("web server get conf fail.", err)
+    }else{
+        port,err := CnfObj.Int("web::web.port")
+        if err!= nil {
+            ServiceError("web server get conf fail.", err)
+        }
+        bind_ip := CnfObj.String("web::web.bind_ip")
+        passwd := CnfObj.String("web::web.passwd")
+
+        fmt.Println(open,port,bind_ip,passwd)
+    }
+
+
     os.Exit(0)
-    //open,err := CnfObj.Int("web::web.enable")
-
-
-    
-
-
 }
 
 func WebHandler() {
