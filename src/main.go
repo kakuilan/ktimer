c23efe44c8e4a74f3f3a377d9702b22341f5abdb
kakuilan <kakuilan@163.com>
2017-04-12 17:50:29 +0800

    pprof

--- src/main.go
+++ src/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"ktimer"
+    "runtime/pprof"
+    "os"
 )
 
 func main() {
+    f, _ := os.Create("profile_file")
+    pprof.StartCPUProfile(f)
+    defer pprof.StopCPUProfile()
+
 	//命令行处理
 	ktimer.CatchCli()
 }
